cfd1c5ac862d4c7f7bdd99509720cf73c3e83c8a
Elisa Flemer <elisaflemer@gmail.com>
2023-09-27 09:52:03 -0300

    deploy

---
+++ static/script.js
@@ -0,0 +1,71 @@
+$('.brasil').hide()
+$('.argentina').hide()
+$('.uruguai').hide()
+$('.franca').hide()
+$('.alemanha').hide()
+
+
+function brasil() {
+    $('.brasil').show()
+    $('.franca').hide()
+    $('.uruguai').hide()
+    $('.alemanha').hide()
+    $('.argentina').hide()
+
+}
+
+
+function franca() {
+    $('.franca').show()
+    $('.brasil').hide()
+    $('.uruguai').hide()
+    $('.argentina').hide()
+    $('.alemanha').hide()
+
+}
+
+
+function uruguai() {
+    $('.uruguai').show()
+    $('.brasil').hide()
+    $('.franca').hide()
+    $('.argentina').hide()
+    $('.alemanha').hide()
+
+}
+
+function alemanha() {
+    $('.alemanha').show()
+    $('.brasil').hide()
+    $('.franca').hide()
+    $('.uruguai').hide()
+    $('.argentina').hide()
+
+}
+
+
+
+function argentina() {
+    $('.argentina').show()
+    $('.brasil').hide()
+    $('.franca').hide()
+    $('.uruguai').hide()
+    $('.alemanha').hide()
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
