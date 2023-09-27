b4ba3a398425f5252edd334bdce4cfe9b50d6469
Elisa Flemer <elisaflemer@gmail.com>
2023-09-27 12:43:03 -0300

    champions

--- jogoscript.js
+++ jogoscript.js
@@ -4,7 +4,7 @@ $('button').click(function() {
     pontos = pontos + 1
     $('#pontuacao').text(pontos)
 
-    // if(pontos > 200) {
-    //     alert('voce ganhou uma champions')
-    // }
+    if(pontos > 200) {
+        alert('voce ganhou uma champions')
+    }
 })
